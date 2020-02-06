d75fcbcbae58a304f09ef7d727374e9f6e623c83
Kitty.Lin <familiar0304@gmail.com>
2020-02-06 13:42:30 +0800

    change port

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.post('/', line.middleware(config), (req, res) => {
 });
 
 // listen on port
-app.listen(3000, () => {
-  console.log('listening on 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
